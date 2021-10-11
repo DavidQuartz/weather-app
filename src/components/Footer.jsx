c7ee541f4f355b3dd17c3cf160b28116a7ed73a7
DavidQuartz <quartzdavid@gmail.com>
2021-10-11 14:06:07 +0000

    Linked in linkedIn in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="footer">
-      created by <span>David Quartey</span>
+      created by{' '}
+      <a
+        href="https://linkedin.com/in/davidquartz"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <span>David Quartey</span>
+      </a>
     </div>
   );
 };
